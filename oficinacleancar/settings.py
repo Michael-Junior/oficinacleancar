72818e43c4caa6e3b6a573bf91bd832af3c56da9
Michael-Junior <michaeljunioroliveira@gmail.com>
2024-01-03 23:37:58 -0300

    Ajuste arquitetura de diretorios e arquivos

--- oficinacleancar/settings.py
+++ oficinacleancar/settings.py
@@ -114,6 +114,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)  
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
